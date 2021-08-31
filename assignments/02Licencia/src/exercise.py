f1c816295dafd209ee4f5fc5369e1a042f285ad3
Kenya Cantu <a01177756@itesm.mx>
2021-08-31 18:47:55 -0500

    commit

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,17 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    id = str(input("Tienes identificación oifical? (s/n)"))
+
+    if (edad >= 18 and id == "s"):
+        print("Trámite de licencia concedido.")
+    elif(edad < 18 or id == "n"):
+            print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
+
+    
+
 
 if __name__ == '__main__':
     main()
