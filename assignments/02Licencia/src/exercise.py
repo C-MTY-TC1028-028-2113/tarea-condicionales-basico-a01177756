75bb4cacbd47e279216375239b620ee9bcb1b989
Kenya Cantu <a01177756@itesm.mx>
2021-09-09 10:08:11 -0500

    listo

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -7,13 +7,16 @@ def main():
         id = str(input("¿Tienes identificación oficial? (s/n): "))
         if id == "s":
             print("Trámite de licencia concedido")
-        else:
+        elif id == "n":
             print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+        
+    elif edad < 18 and edad > 0:
+        print("No cumples requisitos")
     else:
         print("Respuesta incorrecta")
-
-    
-
+   
 
 if __name__ == '__main__':
     main()
